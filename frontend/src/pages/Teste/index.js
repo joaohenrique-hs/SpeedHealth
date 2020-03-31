e207c911e568fbea7d874c8186e4fa88a00abdc2
LuizFerK <luizfernandoklein@live.com>
2020-03-31 09:35:11 -0300

    Initial request from api

--- frontend/src/pages/Teste/index.js
+++ frontend/src/pages/Teste/index.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 // import { Link, useHistory } from 'react-router-dom';
-// import {FiLogIn} from 'react-icons/fi';
+// import {FiLogIn} from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import './style.css';
 
-// import api from '../../services/api';
-
 export default function Teste(){
+    const response = await api.get('/', { message });
+
     return(
         <div className="test-container">
-            <h1> Funcionou </h1>
+            <h1> {response} </h1>
         </div>
     );
 }
