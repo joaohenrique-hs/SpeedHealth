a609447b8942753e4f46b6368ca2449b28cadcee
João Henrique <joaohenrique.hs@hotmail.com>
2020-04-05 21:45:38 -0300

    Upate migrations

--- backend/src/database/migrations/20200401214913_create_user.js
+++ backend/src/database/migrations/20200401214913_create_user.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('users', function (table) {
         table.string('id').primary()
         table.string('name').notNullable()
-        table.string('email').notNullable()
+        table.string('email').notNullable().unique()
         table.string('password').notNullable()
         table.string('whatsapp').notNullable()
         table.string('address').notNullable()
