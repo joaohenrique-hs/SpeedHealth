0b054a0d081141c30868ff43995fda4404ae2b31
João Henrique <joaohenrique.hs@hotmail.com>
2020-04-05 21:27:52 -0300

    Ordered funcition params of generateJwtToken

--- backend/src/utils/generateJwtToken.js
+++ backend/src/utils/generateJwtToken.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const privateKey = process.env.PRIVATE_KEY
 
-module.exports = function generateJwtToken(id, type) {
+module.exports = function generateJwtToken(type, id) {
     const token = jwt.sign({ type: type, id: id}, privateKey, { expiresIn: '72h' })
     return token
 }
