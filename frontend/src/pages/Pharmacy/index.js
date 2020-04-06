7312caa6d674c7a02516ce60f8c18907a2186b23
LuizFerK <luizfernandoklein@live.com>
2020-04-05 21:58:44 -0300

    Responsive pharmacy (register) screen in web

--- frontend/src/pages/Pharmacy/index.js
+++ frontend/src/pages/Pharmacy/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 // import {FiLogIn} from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -50,6 +50,7 @@ export default function Pharmacy(){
     return (
         <div className="pharmacyContainer">
             <div className="form">
+                <img src={Logo} alt='Speed Health'/>
                 <p className="registerTitle">CADASTRO</p>
                 <form onSubmit={handleRegister}>
                     <input 
@@ -93,7 +94,7 @@ export default function Pharmacy(){
                     <div className="inputGroup">
                         <input
                             className="input2"
-                            style={{width: 80}}
+                            style={{width: '20%'}}
                             maxLength="2"
                             placeholder="UF"
                             value={uf}
@@ -101,7 +102,7 @@ export default function Pharmacy(){
                         />
                         <input
                             className="input2"
-                            style={{width: 420}}
+                            style={{width: '80%'}}
                             placeholder="CIDADE"
                             value={city}
                             onChange = {e => setCity(e.target.value)}
@@ -109,9 +110,18 @@ export default function Pharmacy(){
                     </div>
                     <button className="button" type="submit">CADASTRAR-SE</button>
                 </form>
+                <div className="ou">
+                    <hr/>
+                    <p>JÁ TEM CADASTRO?</p>
+                    <hr/>
+                </div>
+                <div className="login">
+                    <Link to="/">
+                        <button className="button"> LOGAR </button>
+                    </Link>
+                </div>
             </div>
             <aside>
-                <img src={Logo} alt='Speed Health'/>
                 <div className="card">
                     <p className="cardTitle">QUEM SOMOS?</p>
                     <p className="cardDescription">A NOSSA PLATAFORMA APRESENTA SOLUÇÃO DE DELIVERY DE MEDICAMENTOS. CONECTE SUA FARMÁCIA COM A SPEED HEALTH E DIGITALIZE SUAS ENTREGAS DE PRODUTOS, POR MEIO DE PAGAMENTO ONLINE E SEGURO E DELIVERY OPERANTE NA SUA CIDADE...</p>
